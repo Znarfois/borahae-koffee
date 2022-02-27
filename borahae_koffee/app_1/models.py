5bd7cda17ffdeb2c05be348cb9cc1f83d2a3e21e
nerijoshua3221 <80408379+nerijoshua3221@users.noreply.github.com>
2022-02-27 21:23:51 +0800

    model for account and created superuser
    Created account data model and created admin superuser
    
    user:bkof
    Pass:123456789

--- borahae_koffee/app_1/models.py
+++ borahae_koffee/app_1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Accounts(models.Model):
+    username = models.CharField(max_length=255,
+    primary_key=True)
+    FirstName = models.CharField(max_length=255)
+    LastName = models.CharField(max_length=255)
+    PhoneNumber = models.PositiveSmallIntegerField()
+    EMail = models.CharField(max_length=255)
+
+
