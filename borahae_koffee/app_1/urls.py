66d5653041b5db312f4292363fc08bc1949c6729
Znarfois <franztaborlupa.tech@gmail.com>
2022-05-28 00:41:04 +0800

    Working with some test cases

--- borahae_koffee/app_1/urls.py
+++ borahae_koffee/app_1/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('create_event', views.create_event, name='create_event'),
     path('create_reservation', views.create_reservation, name='create_reservation'),
     path('create_feedback', views.create_feedback, name='create_feedback'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('event_details/<int:event_id>/', views.update_event, name="update_event"),
+    path('delete_event/<int:event_id>/', views.delete_event, name='delete_event')
 ]
--- borahae_koffee/app_1/views.py
+++ borahae_koffee/app_1/views.py
@@ -24,6 +24,7 @@ def index(request):
     return render(request, 'app_1/MyEvents.html', {"events":events})
 
 def create_event(request):
+    message = ""
     if request.method == 'POST':
         event_id = request.POST.get('event_id')
         name = request.POST.get('event_title')
@@ -35,12 +36,52 @@ def create_event(request):
         participants = request.POST.get('participants')
         max_participants = request.POST.get('max_participants')
         status = request.POST.get('status')
+        if Event.objects.filter(EventStartDate=start_date).exists():
+            message="Date already taken. Please try again"
+            return render(request, 'app_1/create-event-flow-1.html', {'message': message})
+        if start_date > end_date:
+            message="Start date must be before End date. Please try again"
+            return render(request, 'app_1/create-event-flow-1.html', {'message': message})
+        if int(max_participants) > 999:
+            message="Invalid information. Please try again."
+            return render(request, 'app_1/create-event-flow-1.html', {'message': message})
         Event.objects.create(EventID=event_id, EventName=name, EventDescription=description, EventAvailableSlots=participants, EventMaximumCapacity=max_participants, EventStartDate=start_date, EventEndDate=end_date, EventStartTime=start_time, EventEndTime=end_time, EventStatus=status)
-    # return render(request, 'app_1/create-event-flow-1.html')
-    return redirect('create_reservation')
+        return redirect('create_reservation')
+    return render(request, 'app_1/create-event-flow-1.html', {'message': message})
+
+def update_event(request, event_id):
+    success = ""
+    if request.method == 'POST':
+        event_id = request.POST.get('event_id')
+        name = request.POST.get('event_title')
+        description = request.POST.get('event_description')
+        start_date = request.POST.get('event_start_date')
+        end_date = request.POST.get('event_end_date')
+        start_time = request.POST.get('event_start_time')
+        end_time = request.POST.get('event_end_time')
+        participants = request.POST.get('participants')
+        max_participants = request.POST.get('max_participants')
+        status = request.POST.get('status')
+        Event.objects.filter(EventID=event_id).update(EventID=event_id, EventName=name, EventDescription=description, EventAvailableSlots=participants, EventMaximumCapacity=max_participants, EventStartDate=start_date, EventEndDate=end_date, EventStartTime=start_time, EventEndTime=end_time, EventStatus=status)
+        success = "Event Updated!"
+        events = Event.objects.all()
+        return render(request, 'app_1/MyEvents.html', {"events":events, 'success':success})
+
+    events = Event.objects.filter(EventID=event_id)
+    return render(request, 'app_1/event_details.html', {'events': events})
+    
 
 def create_reservation(request):
+    if request.method == 'POST':
+        return redirect('create_feedback')
     return render(request, 'app_1/createrev.html')
+
+def delete_event(request, event_id):
+    success = ""
+    Event.objects.filter(EventID=event_id).delete()
+    success = "Event successfully delete!"
+    events = Event.objects.all()
+    return render(request, 'app_1/MyEvents.html', {"events":events, 'success':success})
 
 def create_feedback(request):
     return render(request, 'app_1/feedback system (1).html')
